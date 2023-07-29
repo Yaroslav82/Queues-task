3bcd6da74953b2ae5ce4091b370c10d0facb650d
Yaroslav82 <yargay230607@gmail.com>
2023-07-29 13:21:26 +0300

    Add start method

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -1,5 +1,17 @@
 package tasks;
 
+import service.DataProvider;
+import utils.Printer;
+
+import java.util.Queue;
+
 public abstract class Task implements DataUpdater {
 
+    public void start() {
+        Queue<String> data = DataProvider.getData();
+        Printer<String> printer = new Printer<>();
+        printer.print("Initial data", data);
+        updateData(data);
+        printer.print("Updated data", data);
+    }
 }
